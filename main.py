4b574395f000250523b30ddf11be4a63ea41723c
Kraftygames <vincentalfredbtomas@gmail.com>
2023-02-14 16:53:33 +0800

    Transfered start to main.py. Spaghetti is real.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import tkinter as tk
-from tkinter import filedialog
-from PIL import Image, ImageTk, ImageOps
-import os
-import random
 import images
 import  selection_logic
-
 
 
 def display_images(folder):
@@ -13,13 +7,7 @@ def display_images(folder):
 
 
 def main():
-    my_images = images.get_images_from_folder(r'C:\Users\vince\OneDrive - Malayan Colleges Mindanao (A Mapúa School)\School\S.Y. 2022 - 2023\Student Council\MIECES\Bomber Jacket\hornet\step 2')
-    selected = selection_logic.choose(lst=my_images, qty=2)
-
-    for a in selected:
-        print(a, end=', ')
-
-    print()
+    import program
 
 
 if __name__ == '__main__':

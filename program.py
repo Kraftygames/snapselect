60c80093c03170895989a1326d927de5dba6cb3f
Kraftygames <vincentalfredbtomas@gmail.com>
2023-02-14 13:55:34 +0800

    Added directory browsing.

--- program.py
+++ program.py
@@ -1,19 +1,18 @@
 import tkinter as tk
 import images
 from PIL import ImageTk, Image
-
-
+from tkinter import filedialog
 
 
 def image_click(index):
     image_app.increase_score(image_app.selected_images[index])
-    update_display()
+    update_image_display()
     print(image_app.scores)
-    pass
+    return
 
 
-def update_display():
-    for widget in root.winfo_children():
+def update_image_display():
+    for widget in photo_wrapper.winfo_children():
         widget.destroy()
 
     image_bin.clear()
@@ -21,21 +20,46 @@ def update_display():
 
     left_image = image_app.get_image_object(index=0, resolution=(512, 512))
     image_bin.append(left_image)
-    left_image_display = tk.Label(root, image=image_bin[0], width=512, height=512)
+    left_image_display = tk.Label(photo_wrapper, image=image_bin[0], width=512, height=512)
     left_image_display.grid(row=0, column=0)
     left_image_display.bind("<Button-1>", lambda e: image_click(0))
 
     # right image
     right_image = image_app.get_image_object(index=1, resolution=(512, 512))
     image_bin.append(right_image)
-    right_image_display = tk.Label(root, image=image_bin[1], width=512, height=512)
+    right_image_display = tk.Label(photo_wrapper, image=image_bin[1], width=512, height=512)
     right_image_display.grid(row=0, column=1)
     right_image_display.bind("<Button-1>", lambda e: image_click(1))
 
 
+
+
+
+
 root = tk.Tk()
-image_app = images.ImageSelection(r'C:\Users\vince\OneDrive - Malayan Colleges Mindanao (A Mapúa School)\School\S.Y. 2022 - 2023\Student Council\MIECES\Bomber Jacket\hornet\step 2')
+
+
+title = tk.Label(root, text="This is the title")
+title.pack()
+
+
+
+def file_function():
+    global image_app
+    file_path = '{}'.format(tk.filedialog.askdirectory(title='Browse'))
+    image_app = images.ImageSelection(file_path)
+    update_image_display()
+    return image_app
+
+
+directory_button = tk.Button(root, text='Browse...', command=lambda :file_function())
+
+directory_button.pack()
+
+
+photo_wrapper = tk.Frame(root)
+photo_wrapper.pack()
 image_bin = []
-update_display()
+#update_image_display()
 root.mainloop()
 
